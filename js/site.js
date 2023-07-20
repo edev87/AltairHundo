f74ec01b74209a1755ccfdd5b94698a5e13a0a92
Ernest M. Dicks <ernest.dicks@luxinteractive.com>
2023-07-20 17:10:00 -0500

    Finished most of the javasciript code, need to add validation still

--- js/site.js
+++ js/site.js
@@ -1,13 +1,56 @@
-function displayMessage(){
-  let inputBox = document.getElementById("message");
-  let message = inputBox.value
+// Get numbers from our form inputs
+//ENTRY POINT
+//Controller function
+function getValues() {
+  let startNum = document.getElementById("start-value").value;  //out of scope, in a different scope
+  let endNum = document.getElementById("end-value").value;
 
-  Swal.fire(
+  startNum = Number(startNum);
+  endNum = Number(endNum);
+
+  let numberArray = generateNumbers(startNum, endNum)
+  displayNumbers(numberArray);
+
+}
+
+//Business logic - creates every number in the input range
+//Data Model
+function generateNumbers(start, end) {  //start and end are parameters
+
+  let range = [];
+  
+
+  for (let number = start; number <= end; number++) {
+    range.push(number);
+  }
+  return range;
+}
+
+//put the numbers of page
+//View Function
+const displayNumbers = (numbersToDisplay) => {
+
+  let tableHtml = '';
+
+  for (let index = 0; index < numbersToDisplay.length; index++) {
+    let currentNumber = numbersToDisplay[index];
+
+    let className = ''
+
+    if(currentNumber % 2 === 0)
     {
-  backdrop: false,
-  title: 'App Name',
-  text: message
+      className = 'even';
+
+    }else{
+      className = 'odd';
     }
-    );
+
+    let tableRowHtml = `<tr><td class="${className}"> ${currentNumber} </td></tr>`;
+    tableHtml = tableHtml + tableRowHtml;
+
+  }
+
+  document.getElementById("results").innerHTML = tableHtml;
+
 
 }
